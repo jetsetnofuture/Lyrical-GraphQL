abec764b3f1763e4974401bfb38453b7b04720b8
Daniel Carbaugh <daniel.carbaugh142@gmail.com>
2024-01-07 23:53:52 -0500

    Routed the Song Details page to the Song List page and added basic navigation between the two pages.

--- client/components/SongDetail.js
+++ client/components/SongDetail.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
+import { Link } from 'react-router';
 import fetchSongDetails from '../queries/fetchSongDetails';
 
 class SongDetail extends Component {
   render() {
-    console.log(this.props);
+    const { song } = this.props.data;
+
+    if (!song) {
+      return <div>Loading...</div>;
+    }
+
     return (
       <div>
-        <h3>Song Detail</h3>
+        <Link to="/">Back</Link>
+        <h3>{song.title}</h3>
       </div>
     );
   }
