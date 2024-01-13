02c1493b8efc9a49d2722261b4c3a5623b78370a
Daniel Carbaugh <daniel.carbaugh142@gmail.com>
2024-01-12 23:36:00 -0500

    Added an optimistic response to the like button mutation so that the number of likes changes instantly after click.

--- client/components/LyricList.js
+++ client/components/LyricList.js
@@ -3,9 +3,17 @@ import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
 
 class LyricList extends Component {
-  onLike(id) {
+  onLike(id, likes) {
     this.props.mutate({
       variables: { id },
+      optimisticResponse: {
+        __typename: 'Mutation',
+        likeLyric: {
+          id,
+          __typename: 'LyricType',
+          likes: likes + 1,
+        },
+      },
     });
   }
 
@@ -15,7 +23,10 @@ class LyricList extends Component {
         <li key={id} className="collection-item">
           {content}
           <div className="vote-box">
-            <i className="material-icons" onClick={() => this.onLike(id)}>
+            <i
+              className="material-icons"
+              onClick={() => this.onLike(id, likes)}
+            >
               thumb_up
             </i>
             {likes}
